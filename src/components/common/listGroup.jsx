59caee53d97b0a57d6b8ca627699a8cbdb23183b
Ragheed Qasmieh <ragheed99@hotmail.com>
2020-08-11 13:26:17 -0400

    Added clickble class to the list group component.

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -12,7 +12,9 @@ const ListGroup = ({
       {items.map((item) => (
         <li
           className={
-            item === selectedItem ? "list-group-item active" : "list-group-item"
+            item === selectedItem
+              ? "clickable list-group-item active"
+              : "clickable list-group-item"
           }
           key={item[valueProperty]}
           onClick={() => onItemSelect(item)}
